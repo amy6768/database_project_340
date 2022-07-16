a1d1aefffd1603a02e1ddd79eed0446256a8d396
apruitt13 <pruittad@oregonstate.edu>
2022-07-16 11:34:50 -0700

    Working on adding data from the webpage.

--- app.js
+++ app.js
@@ -3,14 +3,17 @@
 //setup
 var db = require('./database/db-connector')
 var express = require('express');   // We are using the express library for the web server
+
+
 var app     = express();            // We need to instantiate an express object to interact with the server in our code
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
 PORT        = 9124;                 // Set a port number at the top so it's easy to change in the future
+
 const { engine } = require('express-handlebars');
 var exphbs = require('express-handlebars');     // Import express-handlebars
 app.engine('.hbs', engine({extname: ".hbs"}));  // Create an instance of the handlebars engine to process templates
 app.set('view engine', '.hbs');                 // Tell express to use the handlebars engine whenever it encounters a *.hbs file.
-
-
 
 
 /*
@@ -25,6 +28,56 @@ app.get('/', function(req, res)
             res.render('index', {data: rows});                  // Render the index.hbs file, and also send the renderer
         })                                                      // an object where 'data' is equal to the 'rows' we
     });                                                         // received back from the query                                       // will process this file, before sending the finished HTML to the client.
+
+app.post('/add-student-ajax', function(req, res) 
+    {
+        // Capture the incoming data and parse it back to a JS object
+        let data = req.body;
+    
+        // Capture NULL values
+    
+        let birthdate = parseInt(data.birthdate);
+        if (isNaN(birthdate))
+        {
+            birthdate = 'NULL'
+        }
+    
+        // Create the query and run it on the database
+        query1 = `INSERT INTO Students (studentFirstName, studentLastName, birthdate) VALUES ('${data.studentFirstName}', '${data.studentLastName}', ${homebirthdateworld})`;
+        db.pool.query(query1, function(error, rows, fields){
+    
+            // Check to see if there was an error
+            if (error) {
+    
+                // Log the error to the terminal so we know what went wrong, and send the visitor an HTTP response 400 indicating it was a bad request.
+                console.log(error)
+                res.sendStatus(400);
+            }
+            else
+            {
+                // If there was no error, perform a SELECT * on bsg_people
+                query2 = `SELECT * FROM Students;`;
+                db.pool.query(query2, function(error, rows, fields){
+    
+                    // If there was an error on the second query, send a 400
+                    if (error) {
+                        
+                        // Log the error to the terminal so we know what went wrong, and send the visitor an HTTP response 400 indicating it was a bad request.
+                        console.log(error);
+                        res.sendStatus(400);
+                    }
+                    // If all went well, send the results of the query back.
+                    else
+                    {
+                        res.send(rows);
+                    }
+                })
+            }
+        })
+    });
+
+
+
 /*
     LISTENER
 */

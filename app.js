a3ffebbac7cfbdcc0657803273edf34f14a839fe
apruitt13 <pruittad@oregonstate.edu>
2022-07-16 11:10:23 -0700

    Data is added online for students.

--- app.js
+++ app.js
@@ -1,26 +1,30 @@
 // App.js
 
 //setup
-
+var db = require('./database/db-connector')
+var express = require('express');   // We are using the express library for the web server
+var app     = express();            // We need to instantiate an express object to interact with the server in our code
+PORT        = 9124;                 // Set a port number at the top so it's easy to change in the future
 const { engine } = require('express-handlebars');
 var exphbs = require('express-handlebars');     // Import express-handlebars
 app.engine('.hbs', engine({extname: ".hbs"}));  // Create an instance of the handlebars engine to process templates
 app.set('view engine', '.hbs');                 // Tell express to use the handlebars engine whenever it encounters a *.hbs file.
 
-// Database
-var db = require('./database/db-connector')
 
-var express = require('express');   // We are using the express library for the web server
-var app     = express();            // We need to instantiate an express object to interact with the server in our code
-PORT        = 9124;                 // Set a port number at the top so it's easy to change in the future
+
 
 /*
     ROUTES
 */
 app.get('/', function(req, res)
-    {
-        res.render('index');                    // Note the call to render() and not send(). Using render() ensures the templating engine
-    });                                         // will process this file, before sending the finished HTML to the client.
+    {  
+        let query1 = "SELECT * FROM Students;";               // Define our query
+
+        db.pool.query(query1, function(error, rows, fields){    // Execute the query
+
+            res.render('index', {data: rows});                  // Render the index.hbs file, and also send the renderer
+        })                                                      // an object where 'data' is equal to the 'rows' we
+    });                                                         // received back from the query                                       // will process this file, before sending the finished HTML to the client.
 /*
     LISTENER
 */

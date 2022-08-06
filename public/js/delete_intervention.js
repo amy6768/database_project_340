e40e47deb1fb608e4f71314b2374081f49f07c26
amy6768 <leeamy@oregonstate.edu>
2022-08-06 13:39:39 -0700

    add code for delete intervention

---
+++ public/js/delete_intervention.js
@@ -0,0 +1,47 @@
+// Citation for the CRUD operations:
+//  Date: 7/28/22
+//  Adapted from:
+//  Source URL: https://github.com/osu-cs340-ecampus/nodejs-starter-app
+
+function deleteIntervention(idIntervention) {
+    // Put our data we want to send in a javascript object
+    let data = {
+        id: idIntervention
+    };
+
+    // Setup our AJAX request
+    var xhttp = new XMLHttpRequest();
+    xhttp.open("DELETE", "/delete-intervention-ajax", true);
+    xhttp.setRequestHeader("Content-type", "application/json");
+    console.log(data)
+    console.log(xhttp)
+    // Tell our AJAX request how to resolve
+    xhttp.onreadystatechange = () => {
+        if (xhttp.readyState == 4 && xhttp.status == 204) {
+
+            // Add the new data to the table
+            deleteRow(idIntervention);
+
+        }
+        else if (xhttp.readyState == 4 && xhttp.status != 204) {
+            console.log("There was an error with the input.")
+        }
+    }
+    // Send the request and wait for the response
+    xhttp.send(JSON.stringify(data));
+}
+
+
+function deleteRow(idIntervention){
+
+    let table = document.getElementById("intervention-table");
+    for (let i = 0, row; row = table.rows[i]; i++) {
+       //iterate through rows
+       //rows would be accessed using the "row" variable assigned in the for loop
+       if (table.rows[i].getAttribute("data-value") == idIntervention) {
+            table.deleteRow(i);
+            break;
+       }
+    }
+}
+
